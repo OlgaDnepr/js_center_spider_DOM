7ee0423052f8abe3ff639c9de233c8e5fcfaa928
Your Name <cocochoco.story@gmail.com>
2026-07-10 20:08:25 +0300

    make build

---
+++ js_center_spider_DOM.1da1ca94.js
@@ -0,0 +1,2 @@
+"use strict";var wall=document.querySelector(".wall"),spider=document.querySelector(".spider");if(wall&&spider){var e=wall.clientWidth/2-spider.clientWidth/2,t=wall.clientHeight/2-spider.clientHeight/2;spider.style.left="".concat(e,"px"),spider.style.top="".concat(t,"px")}
+//# sourceMappingURL=js_center_spider_DOM.1da1ca94.js.map
